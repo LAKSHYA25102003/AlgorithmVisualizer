812ea55559ea602e9f309806b0ae16cda425af58
LAKSHYA <llakshya63@gmail.com>
2023-02-03 15:34:10 +0530

    Basic navbar and landing page is created

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import LandingNavbar from "../Components/Navbar/LandingNavbar";
+
+function Landing() {
+  return (
+    <div>
+      <LandingNavbar/>
+      <div className="flex justify-center content-start">
+        <div className="w-[100%] bg-blue-600 min-h-screen">
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
